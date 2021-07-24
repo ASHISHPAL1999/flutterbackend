b19a653ad79c90f61642c980e46e9e6bfff9466d
ASHISH PAL <68744952+ASHISHPAL1999@users.noreply.github.com>
2021-07-24 09:52:04 +0530

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express=require("express")
 const router = require("./Controller/router")
 const app=express()
 require('dotenv').config()
+const cors=require("cors")
 const route=require('./Controller/router')
 const dbconnect=require('./Controller/dbconnect')
 
 const host="localhost"
- 
+ app.use(cors());
 const port=process.env.PORT || 2000
 app.use(router);
 
