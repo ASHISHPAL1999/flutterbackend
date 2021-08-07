3af25b38541c980f0e9455c5e6d9874f1fec1c2b
ASHISH PAL <68744952+ASHISHPAL1999@users.noreply.github.com>
2021-08-07 18:26:36 +0530

    message

--- Controller/router.js
+++ Controller/router.js
@@ -1,10 +1,15 @@
 const express=require("express")
 const app=express();
-const { Mongoose } = require("mongoose")
+const { Mongoose, isValidObjectId } = require("mongoose")
 const router=express.Router()
 const database=require('./Model/userSignUp')
 const usercontent=require("./Model/userdata")
 const jwt=require("jsonwebtoken")
+var validator = require('validator');
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
   
 router.get("/",(req,res)=>{
     res.send("Server running");
@@ -27,7 +32,8 @@ router.post("/register",express.json(), (req,res)=>{
     console.log("user not exist block")
     db.save().then((success)=>{
        
-        res.status(201).send(db)
+        res.status(201).send({"_id":db._id,"userName":db.userName,"userEmail":db.userEmail
+    })
 
     }).catch((err)=>{
         res.status(400).send(err);
@@ -94,6 +100,7 @@ router.post("/signin",express.json(),(req,res)=>{
 
 
 router.get("/user",express.json(), (req,res)=>{
+    
   database.findOne({token:req.headers.authorization}).then((result)=>{
       if(result==null){
           res.status(404).send("invalid token")
@@ -101,11 +108,73 @@ router.get("/user",express.json(), (req,res)=>{
       else{
           
         usercontent.findOne({userEmail:result.userEmail}).then((val)=>{
+            
+
+           if(val==null){
+               res.status(404).send("user not found")
+           }
+           else{
             res.status(200).send(val)
+           }
         }).catch((err)=>{console.log(err)})
           
       }
   })
 
 })
+
+router.put("/user",express.json(),(req,res)=>{
+console.log(req.headers)
+console.log(req.body)
+
+    if(req.headers.authorization==null){
+
+        res.status(400).send("Invalid token")
+        
+    }
+    else{
+        if(validator.isEmail(req.body.userEmail)){
+           
+            database.findOne({token:req.headers.authorization}).then((result)=>{
+                var email=result.userEmail
+                database.findOne({token:req.headers.authorization}).then((result)=>{
+                    if(result==null){
+                        res.status(404).send("Invalid token")
+                    }
+                    else{
+                        database.updateOne({token:req.headers.authorization},{$set:{userName:req.body.userName,userEmail:req.body.userEmail}}).then((val)=>{
+                            usercontent.updateOne({userEmail:email},{$set:{userName:req.body.userName,userEmail:req.body.userEmail}}).then((result)=>{
+                                res.send("updation done both places");
+                            }).catch((err)=>{res.send("server error 1")})
+                    
+                            
+                        }).catch((err)=>{res.send("server error 2")})
+                    }
+                }).catch((err)=>{res.send("server error 3")})
+                
+            }).catch((err)=>{res.send("server error 4")})
+        }
+        else{
+            res.status(400).send("invalid email format")
+        }
+
+    }
+})
+
+router.delete("/user",express.json(), (req,res)=>{
+    database.findOne({token:req.headers.authorization}).then((result)=>{
+        if(result==null){
+            res.status(404).send("invalid token or user not exist")
+        }
+        else{
+            
+          usercontent.deleteOne({userEmail:result.userEmail}).then((val)=>{
+              res.status(200).send("user deleted successfully")
+          }).catch((err)=>{console.log(err)})
+            
+        }
+    })
+  
+  })
+
 module.exports =router;
